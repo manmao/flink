c877902ba1dffba8960fb48d947245809e61c713
manmao <manmao>
2023-07-05 14:08:20 +0800

    feature: 增加mvn打包命令

--- mvnCmd.sh
+++ mvnCmd.sh
@@ -1,2 +1,10 @@
  # 快速编译
 mvn clean install -DskipTests -Dfast -Pskip-webui-build -T 1C
+
+#仓库地址，使用aliyun maven仓库
+    <mirror>
+      <id>alimaven</id>
+      <name>aliyun maven</name>
+      <url>http://maven.aliyun.com/nexus/content/groups/public/</url>
+      <mirrorOf>central</mirrorOf>
+    </mirror>
